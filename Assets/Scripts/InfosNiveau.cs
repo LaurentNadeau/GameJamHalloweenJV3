2d5616ddddd6244129e0aa045ff0bd8034c011f4
janieal <janieaubertl@gmail.com>
2023-10-24 09:42:33 -0400

    Décompte

---
+++ Assets/Scripts/InfosNiveau.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "InfosNiveau", menuName = "ScriptableObjects/InfosNiveau")]
+public class InfosNiveau : ScriptableObject
+{
+    public float temps;
+    public float tempsInitial;
+}
